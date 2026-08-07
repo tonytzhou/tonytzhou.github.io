8dcdf6434f8f79dcbf0877a22073f8b9d09ecae3
Tony Zhou <tzhou070@ucr.edu>
2026-08-06 22:03:38 -0300

    test

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -2,20 +2,12 @@ import { Message } from "./message";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import Home from "./routes/Home";
 
 function App() {
   return (
     <div>
-      {" "}
-      {/* {
-        <ListGroup
-          items={items}
-          heading="Cities"
-          onSelectItem={handleSelectItem}
-        />
-      } */}
-      {/* <Alert text={text} /> */}
-      <Button onClick={() => console.log("you clciked me!")}>hi :3</Button>
+      <Home />
     </div>
   );
 }
